98888172026b10d4d81cc3a3f9a239469135e396
fabio <fabiodg86@hotmail.it>
2025-01-17 17:46:26 +0100

    review

--- modules/2.more_basics_oop/9.oop.js
+++ modules/2.more_basics_oop/9.oop.js
@@ -1,11 +1,11 @@
 /*
-        1.Dichiarazione della classe   -14
-        2. definire le proprietà della classe -16/18
-        3. definire il costruttore della classe  -21
-        4. referenza delle proprietà    -23/25
+        1.Dichiarazione della classe   -11
+        2. definire le proprietà della classe -12/14
+        3. definire il costruttore della classe  -16
+        4. referenza delle proprietà    -19/21
         5. creare l'oggetto o istanza della classe passando al costruttore 
-            i valori delle proprietà precedentemente referenziate dal costruttore -30
-        6. stampa dei valori delle proprietà mediante l'istanza della classe  -31
+            i valori delle proprietà precedentemente referenziate dal costruttore -26
+        6. stampa dei valori delle proprietà mediante l'istanza della classe  -27
 
                */
 class Studente {
