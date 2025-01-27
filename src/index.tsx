f4fa1837f6fd3673e1cfaa51fee8fac0fe599fa2
fabio <fabiodg86@hotmail.it>
2025-01-27 16:34:14 +0100

    review_1

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 
 import reportWebVitals from './reportWebVitals';
-import TestUnitTestAsAsync from './12-TestUnit/02-testing-async-code/components/TestUnitTestAsAsync';
 import App from './App';
 
 
