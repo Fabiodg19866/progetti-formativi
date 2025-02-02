ce1b40a309ce33db8863c7600a2d77489a3e4544
fabio <fabiodg86@hotmail.it>
2025-02-02 17:52:29 +0100

    review_5

--- modules/2.more_basics_oop/test.js
+++ modules/2.more_basics_oop/test.js
@@ -1,25 +1,18 @@
-   
 class Studente {
-     
-    nome = ''
-    cognome = ''
+  nome = "";
+  cognome = "";
 
-  constructor(nome,cognome) {
-      
-    this.nome = nome
-    this.cognome = cognome
-
+  constructor(nome, cognome) {
+    this.nome = nome;
+    this.cognome = cognome;
   }
-  
-  getName() {
-      
-    return this.nome
-    }
 
-   }
-    
-let studente = new Studente('fabio', 'di gloria')
-let getName = studente.getName()
+  getName() {
+    return this.nome;
+  }
+}
+
+let studente = new Studente("fabio", "di gloria");
+let getName = studente.getName();
 console.log(studente);
 console.log(getName);
-   
