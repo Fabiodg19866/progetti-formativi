d00e5ab32addba7703fdc665b5b2cd8631e057a6
fabio <fabiodg86@hotmail.it>
2025-06-09 09:17:58 +0200

    features-typescript

--- modules/10.intersection-types/intersection-types.js
+++ modules/10.intersection-types/intersection-types.js
@@ -1,8 +1,7 @@
-const employe = {
+var employe = {
     name: 'Max',
     employe: 'Technical Lead',
     privileges: ['Coordinator Team'],
     startDate: new Date()
 };
 console.log(employe);
-//# sourceMappingURL=intersection-types.js.map
