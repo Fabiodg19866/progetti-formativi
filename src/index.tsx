06364c6604c32c9175778e3a399fd6626c008a91
fabio <fabiodg86@hotmail.it>
2026-07-25 18:38:36 +0200

    fix-react-2

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import reportWebVitals from "./reportWebVitals";
-import User from "./11-Spring-React/User";
+import App from "./App";
+
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement,
 );
 root.render(
   <React.StrictMode>
-    <User />
+    <App/>
   </React.StrictMode>,
 );
 
